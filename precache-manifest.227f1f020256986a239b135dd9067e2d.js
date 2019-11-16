864a89d2a314a6e8ca4e4a6581218e5b23eaa9d3
exegate <der.shoxed@gmail.com>
2019-11-16 19:41:26 +0300

    Updates

---
+++ precache-manifest.227f1f020256986a239b135dd9067e2d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3cf74254f7cdb080c06379a17a0fffc6",
+    "url": "/testte/index.html"
+  },
+  {
+    "revision": "d8a632ac1494bf02b916",
+    "url": "/testte/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "b76b80b18797611de4de",
+    "url": "/testte/static/css/main.9c1e8651.chunk.css"
+  },
+  {
+    "revision": "d8a632ac1494bf02b916",
+    "url": "/testte/static/js/2.df596efa.chunk.js"
+  },
+  {
+    "revision": "b76b80b18797611de4de",
+    "url": "/testte/static/js/main.81c5706d.chunk.js"
+  },
+  {
+    "revision": "0c6b9ea3fedd8b88c3fc",
+    "url": "/testte/static/js/runtime-main.9db878b8.js"
+  }
+]);
